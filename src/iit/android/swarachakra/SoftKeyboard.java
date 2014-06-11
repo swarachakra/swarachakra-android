679e7b855e8eae3cb7392a92b7055a4176d7f325
Madhu kiran <p.madhu.kiran96@gmail.com>
2014-06-11 13:29:06 +0530

    marathi language added

--- src/iit/android/swarachakra/SoftKeyboard.java
+++ src/iit/android/swarachakra/SoftKeyboard.java
@@ -3,8 +3,8 @@ package iit.android.swarachakra;
 import iit.android.language.ExceptionHandler;
 import iit.android.language.Language;
 import iit.android.language.english.English;
-import iit.android.language.hindi.MainLanguage;
-import iit.android.language.hindi.MainLanguageExceptionHandler;
+import iit.android.language.marathi.MainLanguage;
+import iit.android.language.marathi.MainLanguageExceptionHandler;
 
 import java.util.HashMap;
 import java.util.List;
