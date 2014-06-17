8dc2fe0158c89f24004806f7c1f520e65c2a551a
Manideep Polireddi <manideepiitb@gmail.com>
2014-06-18 02:04:16 +0530

    Added extra symbols in english, changing shift icon

--- src/iit/android/swarachakra/EnglishKeyboardActionListener.java
+++ src/iit/android/swarachakra/EnglishKeyboardActionListener.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.inputmethodservice.Keyboard.Key;
 import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
 import android.os.Handler;
@@ -24,7 +25,7 @@ import android.widget.TextView;
 
 public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 		OnTouchListener {
-	
+
 	private int ENTER;
 	private int BACKSPACE;
 	private int SPACE;
@@ -37,29 +38,29 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 	private InputConnection mInputConnection;
 	private List<Integer> mSpecialKeys;
 	private static HashMap<Integer, Key> mKeyboardKeys;
-	
+
 	private static final int MSG_SHOW_PREVIEW = 1;
 	private static final int MSG_REMOVE_PREVIEW = 2;
 	private static final int DELAY_BEFORE_PREVIEW = 0;
 	private static final int DELAY_AFTER_PREVIEW = 140;
-	
+
 	private static PopupWindow mPreviewPopup;
 	private static TextView mPreviewTextView;
-	
-	
+
 	private boolean isShifted;
 	private boolean inSymbolMode;
+	private boolean inMoreSymbolMode;
 	private boolean isPersistent;
 	private boolean spaceHandled;
 	private boolean shiftHandled;
 	private boolean inQuickSymbolMode;
-	
+
 	@SuppressLint("HandlerLeak")
-	Handler mHandler = new Handler(){
+	Handler mHandler = new Handler() {
 		@SuppressLint("NewApi")
 		@Override
-		public void handleMessage(Message msg){
-			switch(msg.what){
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
 			case MSG_SHOW_PREVIEW:
 				showPreview(msg.arg1);
 				break;
@@ -69,10 +70,10 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 			}
 		}
 	};
-	
-	public void initialize(EnglishKeyboardView kv){
+
+	public void initialize(EnglishKeyboardView kv) {
 		mKeyboardView = kv;
-		
+
 		BACKSPACE = mKeyboardView.BACKSPACE;
 		ENTER = mKeyboardView.ENTER;
 		SPACE = mKeyboardView.SPACE;
@@ -80,35 +81,37 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 		SYMBOLS = mKeyboardView.SYMBOLS;
 		SHIFT = mKeyboardView.SHIFT;
 		mSpecialKeys = new ArrayList<Integer>();
-		mSpecialKeys = Arrays.asList(BACKSPACE, ENTER, SPACE, ENGLISH, SYMBOLS, SHIFT);
+		mSpecialKeys = Arrays.asList(BACKSPACE, ENTER, SPACE, ENGLISH, SYMBOLS,
+				SHIFT);
 		buildKeyboardKeys();
-		
+
 		mPreviewPopup = mKeyboardView.mPreviewPopup;
 		mPreviewTextView = mKeyboardView.mPreviewTextView;
-		
+
 		isShifted = false;
 		inSymbolMode = false;
+		inMoreSymbolMode = false;
 		isPersistent = false;
 		inQuickSymbolMode = false;
 	}
-	
+
 	@SuppressLint("UseSparseArrays")
-	private static void buildKeyboardKeys(){
+	private static void buildKeyboardKeys() {
 		mKeyboardKeys = new HashMap<Integer, Key>();
 		List<Key> keys = mKeyboardView.getKeyboard().getKeys();
-		for(Key key:keys){
+		for (Key key : keys) {
 			mKeyboardKeys.put(key.codes[0], key);
 		}
 	}
-	
+
 	public void setSoftKeyboard(SoftKeyboard sk) {
 		this.mSoftKeyboard = sk;
 	}
-	
+
 	public void setKeysMap(HashMap<Integer, KeyAttr> mKeys) {
 		this.mKeys = mKeys;
 	}
-	
+
 	public void setInputConnection(InputConnection ic) {
 		mInputConnection = ic;
 	}
@@ -116,18 +119,25 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 	@Override
 	public boolean onTouch(View v, MotionEvent me) {
 		int action = me.getActionMasked();
-		if(action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_POINTER_UP){
-			mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_REMOVE_PREVIEW), DELAY_AFTER_PREVIEW);
-		}
-		else if(action == MotionEvent.ACTION_MOVE){
+		if (action == MotionEvent.ACTION_UP
+				|| action == MotionEvent.ACTION_POINTER_UP) {
+			mHandler.sendMessageDelayed(
+					mHandler.obtainMessage(MSG_REMOVE_PREVIEW),
+					DELAY_AFTER_PREVIEW);
+		} else if (action == MotionEvent.ACTION_MOVE) {
 			List<Key> keys = mKeyboardView.getKeyboard().getKeys();
-			for(Key key:keys){
-				if(key.pressed == true && !(mSpecialKeys.contains(key.codes[0]))){
+			for (Key key : keys) {
+				if (key.pressed == true
+						&& !(mSpecialKeys.contains(key.codes[0]))) {
 					mHandler.removeMessages(MSG_REMOVE_PREVIEW);
-					mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_SHOW_PREVIEW, key.codes[0], 0), DELAY_BEFORE_PREVIEW);
-				}
-				else if(key.pressed == true && (mSpecialKeys.contains(key.codes[0]))){
-					mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_REMOVE_PREVIEW), DELAY_AFTER_PREVIEW);
+					mHandler.sendMessageDelayed(mHandler.obtainMessage(
+							MSG_SHOW_PREVIEW, key.codes[0], 0),
+							DELAY_BEFORE_PREVIEW);
+				} else if (key.pressed == true
+						&& (mSpecialKeys.contains(key.codes[0]))) {
+					mHandler.sendMessageDelayed(
+							mHandler.obtainMessage(MSG_REMOVE_PREVIEW),
+							DELAY_AFTER_PREVIEW);
 				}
 			}
 		}
@@ -136,74 +146,75 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 
 	@Override
 	public void onKey(int arg0, int[] arg1) {
-		
+
 	}
 
 	@Override
 	public void onPress(int keyCode) {
 		spaceHandled = false;
 		shiftHandled = false;
-		if(keyCode == SYMBOLS){
+		if (keyCode == SYMBOLS) {
 			inQuickSymbolMode = true;
 			handleSpecialInput(SYMBOLS);
 			Log.d("testing", "on press shift");
 		}
-		if(!(mSpecialKeys.contains(keyCode))){
+		if (!(mSpecialKeys.contains(keyCode))) {
 			mHandler.removeMessages(MSG_REMOVE_PREVIEW);
-			mHandler.sendMessageDelayed(mHandler.obtainMessage(MSG_SHOW_PREVIEW, keyCode, 0), DELAY_BEFORE_PREVIEW);
+			mHandler.sendMessageDelayed(
+					mHandler.obtainMessage(MSG_SHOW_PREVIEW, keyCode, 0),
+					DELAY_BEFORE_PREVIEW);
 		}
 	}
 
 	@Override
 	public void onRelease(int keyCode) {
 		String label = getLabel(keyCode);
-		if(mKeys.containsKey(keyCode)){
+		if (mKeys.containsKey(keyCode)) {
 			if (isShifted && !(isPersistent)) {
 				isShifted = false;
 				changeLayout();
 			}
 			commitText(label);
-			if(inQuickSymbolMode){
+			if (inQuickSymbolMode) {
 				handleSpecialInput(SYMBOLS);
 			}
-		}
-		else{
-			if(keyCode != SYMBOLS){
+		} else {
+			if (keyCode != SYMBOLS) {
 				handleSpecialInput(keyCode);
 			}
 		}
 		inQuickSymbolMode = false;
-		
+
 	}
-	
-	private void showPreview(int keyCode){
-		if(keyCode != 0){
-			
+
+	private void showPreview(int keyCode) {
+		if (keyCode != 0) {
+
 			Key key = mKeyboardKeys.get(keyCode);
 			int w = 0;
 			int h = 0;
 			int x = 0;
 			int y = 0;
-			w = key.width*5/3;
-			h = key.height*5/3;
-			x = key.x - w/5;
-			y = key.y - h + (int)0.1*key.y;
-			
+			w = key.width * 5 / 3;
+			h = key.height * 5 / 3;
+			x = key.x - w / 5;
+			y = key.y - h + (int) 0.1 * key.y;
+
 			mPreviewTextView.setText(getLabel(keyCode));
-			
-			if(mPreviewPopup.isShowing()){
+
+			if (mPreviewPopup.isShowing()) {
 				mPreviewPopup.update(x, y, w, h);
-			}
-			else{
+			} else {
 				mPreviewPopup.setWidth(w);
 				mPreviewPopup.setHeight(h);
-				mPreviewPopup.showAtLocation(mKeyboardView, Gravity.NO_GRAVITY, x, y);
+				mPreviewPopup.showAtLocation(mKeyboardView, Gravity.NO_GRAVITY,
+						x, y);
 			}
 			mPreviewTextView.setVisibility(View.VISIBLE);
 		}
 	}
-	
-	private void removePreview(){
+
+	private void removePreview() {
 		mPreviewTextView.setVisibility(View.GONE);
 	}
 
@@ -236,20 +247,22 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 		removePreview();
 
 	}
-	
+
 	public void onLongPress(Key key) {
 		if (key.codes[0] == SHIFT) {
-			if (isShifted && isPersistent) {
-				isPersistent = false;
-				isShifted = false;
-				changeLayout();
-			} else {
-				isPersistent = true;
-				isShifted = true;
-				changeLayout();
+			if(!inSymbolMode){
+				if (isShifted && isPersistent) {
+					isPersistent = false;
+					isShifted = false;
+					changeLayout();
+				} else {
+					isPersistent = true;
+					isShifted = true;
+					changeLayout();
+				}
+				shiftHandled = true;
 			}
-			shiftHandled = true;
-			
+
 		} else if (key.codes[0] == SPACE) {
 			InputMethodManager imm = (InputMethodManager) mSoftKeyboard
 					.getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -257,17 +270,19 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 			spaceHandled = true;
 		}
 	}
-	
-	private String getLabel(int keyCode){
-		if(mKeys.containsKey(keyCode)){
-			if(inSymbolMode){
-				if(keyCode == 53){
-					return ",";
+
+	private String getLabel(int keyCode) {
+		if (mKeys.containsKey(keyCode)) {
+			if (inSymbolMode) {
+				if (keyCode == 53) {
+					return ".";
+				}
+				if(inMoreSymbolMode){
+					return mKeys.get(keyCode + 80).label;
 				}
 				return mKeys.get(keyCode + 54).label;
-			}
-			else if(isShifted){
-				if(keyCode == 53){
+			} else if (isShifted) {
+				if (keyCode == 53) {
 					return ".";
 				}
 				return mKeys.get(keyCode + 26).label;
@@ -276,7 +291,7 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 		}
 		return "";
 	}
-	
+
 	/**
 	 * Changes the layout of the keyboard
 	 * 
@@ -285,14 +300,48 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 	 */
 	private void changeLayout() {
 		List<Key> keys = mKeyboardView.getKeyboard().getKeys();
-		for(Key key: keys){
-			if(mKeys.containsKey(key.codes[0])){
+		for (Key key : keys) {
+			if (mKeys.containsKey(key.codes[0])) {
 				key.label = getLabel(key.codes[0]);
+			} else {
+				int code = key.codes[0];
+				if (code == SHIFT) {
+
+					if (!(inSymbolMode)) {
+
+						if (!isPersistent) {
+							Drawable normalShift = mSoftKeyboard.getResources()
+									.getDrawable(R.drawable.shift);
+							key.icon = normalShift;
+							key.label = null;
+						} else {
+							Drawable activeShift = mSoftKeyboard.getResources()
+									.getDrawable(R.drawable.shift_active);
+							key.icon = activeShift;
+							key.label = null;
+						}
+
+					} else {
+						if (inMoreSymbolMode) {
+							key.label = "@#$";
+							key.icon = null;
+						} else {
+							key.label = "<~+";
+							key.icon = null;
+						}
+					}
+				} else if (code == SYMBOLS) {
+					if (inSymbolMode) {
+						key.label = "ABC";
+					} else {
+						key.label = "123";
+					}
+				}
 			}
 		}
 		mKeyboardView.invalidateAllKeys();
 	}
-	
+
 	/**
 	 * Changes the language of the keyboard
 	 * 
@@ -302,7 +351,7 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 	private void changeLanguage() {
 		mSoftKeyboard.changeLanguage();
 	}
-	
+
 	/**
 	 * Handles what to be done when keys like ENTER, SPACE, BACKSPACE, EN,
 	 * SHIFT, SYM
@@ -321,6 +370,9 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 					isShifted = true;
 					changeLayout();
 				}
+			} else {
+				inMoreSymbolMode = !(inMoreSymbolMode);
+				changeLayout();
 			}
 		}
 
@@ -343,6 +395,7 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 		else if (keyCode == SYMBOLS) {
 			if (inSymbolMode) {
 				inSymbolMode = !(inSymbolMode);
+				inMoreSymbolMode = false;
 				if (isPersistent && isShifted) {
 					changeLayout();
 				} else {
@@ -350,6 +403,7 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 				}
 			} else {
 				inSymbolMode = !(inSymbolMode);
+				inMoreSymbolMode = false;
 				changeLayout();
 			}
 		}
@@ -359,6 +413,7 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 		}
 
 		else if (keyCode == ENTER) {
+
 			handleEnter();
 		}
 
@@ -371,7 +426,7 @@ public class EnglishKeyboardActionListener implements OnKeyboardActionListener,
 		mInputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP,
 				KeyEvent.KEYCODE_ENTER));
 	}
-	
+
 	/**
 	 * Send text to the EditText
 	 * 
