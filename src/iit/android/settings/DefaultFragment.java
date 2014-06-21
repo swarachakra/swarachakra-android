3e8f56c0997c1eee86111230d621e10b887b8749
Manideep Polireddi <manideepiitb@gmail.com>
2014-06-22 02:53:39 +0530

    changed backgounds, key placements, and removed extra lines setup pages

--- src/iit/android/settings/DefaultFragment.java
+++ src/iit/android/settings/DefaultFragment.java
@@ -17,17 +17,14 @@ MainActivity mainApp = MainActivity.getMainApp();
 		
 		View v = inflater.inflate(R.layout.default_fragment, container,  false);
 		TextView instructionsHeadTextView = (TextView)v.findViewById(R.id.default_instructions_head);
-		TextView stepsHeadTextView = (TextView)v.findViewById(R.id.default_steps_head);
 		TextView step1TextView = (TextView)v.findViewById(R.id.default_step_1);
 		Button defaultButton = (Button)v.findViewById(R.id.default_button);
 		
 		String instructionsHeadText = mainApp.getStringResourceByName("default_instructions_head");
-		String stepsHeadText = mainApp.getStringResourceByName("default_steps_head");
 		String step1Text = mainApp.getStringResourceByName("default_step_1");
 		String defaultButtonText = mainApp.getStringResourceByName("default_button");
 		
 		instructionsHeadTextView.setText(instructionsHeadText);
-		stepsHeadTextView.setText(stepsHeadText);
 		step1TextView.setText(step1Text);
 		defaultButton.setText(defaultButtonText);
 		return v;
--- src/iit/android/settings/EnableFragment.java
+++ src/iit/android/settings/EnableFragment.java
@@ -18,21 +18,18 @@ public class EnableFragment extends Fragment {
 		
 		View v = inflater.inflate(R.layout.enable_fragment, container,  false);
 		TextView instructionsHeadTextView = (TextView)v.findViewById(R.id.enable_instructions_head);
-		TextView stepsHeadTextView = (TextView)v.findViewById(R.id.enable_steps_head);
 		TextView step1TextView = (TextView)v.findViewById(R.id.enable_step_1);
 		TextView step2TextView = (TextView)v.findViewById(R.id.enable_step_2);
 		TextView step3TextView = (TextView)v.findViewById(R.id.enable_step_3);
 		Button enableButton = (Button)v.findViewById(R.id.enable_button);
 		
 		String instructionsHeadText = mainApp.getStringResourceByName("enable_instructions_head");
-		String stepsHeadText = mainApp.getStringResourceByName("enable_steps_head");
 		String step1Text = mainApp.getStringResourceByName("enable_step_1");
 		String step2Text = mainApp.getStringResourceByName("enable_step_2");
 		String step3Text = mainApp.getStringResourceByName("enable_step_3");
 		String enableButtonText = mainApp.getStringResourceByName("enable_button");
 		
 		instructionsHeadTextView.setText(instructionsHeadText);
-		stepsHeadTextView.setText(stepsHeadText);
 		step1TextView.setText(step1Text);
 		step2TextView.setText(step2Text);
 		step3TextView.setText(step3Text);
