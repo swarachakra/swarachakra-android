6b5776b07e4e9863add2793ab90193e839efeb2d
Nagraj Emmadi <Nagraj Emmadi>
2015-10-26 17:42:42 +0530

    Konkani ported to android studio

--- app/src/konkani/java/iit/android/language/MainLanguageExceptionHandler.java
+++ app/src/konkani/java/iit/android/language/MainLanguageExceptionHandler.java
@@ -1,4 +1,4 @@
-package iit.android.language.konkani;
+package iit.android.language;
 
 import iit.android.language.ExceptionHandler;
 import iit.android.language.Language;
@@ -10,42 +10,43 @@ import java.util.HashMap;
 import android.annotation.SuppressLint;
 
 public class MainLanguageExceptionHandler implements ExceptionHandler {
-	
+
 	private ArrayList<KeyAttr> keyArray;
 	private HashMap<Integer, KeyAttr> mKeys;
 	private Language main;
-	
-	private static final String RA = "\u0930";	
+
+	private static final String RA = "\u0930";
 	private static final String HALANT = "\u094D";
-	private static final String EYELASHRA = "\u0931";
-	
+	private static final String EYELASHRA = "\u0930";
+	private static final String ZWJ = "\u200D";
+
 	private static final int RAFARCODE = 53;
 	private static final int TRAKARCODE = 52;
 	private static final int EYELASHRACODE = 51;
-	
+
 	public MainLanguageExceptionHandler(Language lang){
 		main = lang;
 		initializeKeyArray();
 	}
-	
+
 	@SuppressLint("UseSparseArrays")
 	public HashMap<Integer, KeyAttr> handleException(int keyCode){
 		HashMap<Integer, KeyAttr> sKeys = new HashMap<Integer, KeyAttr>();
 		switch(keyCode){
-		case EYELASHRACODE:
-			handleEyelashRa(sKeys);
-			break;
-		case TRAKARCODE:
-			handleTrakar(sKeys);
-			break;
-		case RAFARCODE:
-			handleRafar(sKeys);
-			break;
+			case EYELASHRACODE:
+				handleEyelashRa(sKeys);
+				break;
+			case TRAKARCODE:
+				handleTrakar(sKeys);
+				break;
+			case RAFARCODE:
+				handleRafar(sKeys);
+				break;
 		}
 		initializeKeyArray();
 		return sKeys;
 	}
-	
+
 	private void initializeKeyArray(){
 		keyArray = new ArrayList<KeyAttr>();
 		int halantEnd = main.halantEnd;
@@ -77,11 +78,11 @@ public class MainLanguageExceptionHandler implements ExceptionHandler {
 
 	private void handleEyelashRa(HashMap<Integer, KeyAttr> sKeys) {
 		for(KeyAttr key : keyArray){
-			String newLabel = EYELASHRA + HALANT + mKeys.get(key.code).label;
+			String newLabel = EYELASHRA + HALANT + ZWJ + mKeys.get(key.code).label;
 			key.label = newLabel;
 			key.showChakra = true;
 			sKeys.put(key.code, key);
 		}
 	}
-	
+
 }
