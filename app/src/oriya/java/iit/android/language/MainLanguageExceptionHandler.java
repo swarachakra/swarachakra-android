b7a9f7b841fd37928155953dbf2b1af3d7621afc
Nagraj Emmadi <Nagraj Emmadi>
2015-10-27 18:51:49 +0530

    Oriya ported to android studio

--- app/src/oriya/java/iit/android/language/MainLanguageExceptionHandler.java
+++ app/src/oriya/java/iit/android/language/MainLanguageExceptionHandler.java
@@ -16,22 +16,24 @@ public class MainLanguageExceptionHandler implements ExceptionHandler {
 	private HashMap<Integer, KeyAttr> mKeys;
 	private Language main;
 
-	private static final String RA = "\u0930";
-	private static final String HALANT = "\u094D";
+	private static final String RA = "\u0B30";
+	private static final String HALANT = "\u0B4D";
 	//private static final String NUKTA = "\u093C";
-	private static final String [] nuktaCharacters = {"\u0958","\u0959","\u095a","\u095b","\u095c","\u095d","\u0929","\u095e","\u095f","\u0931","\u0934"};
+	private static final String [] nuktaCharacters = {"\u0B5c","\u0B5d"};
 
 	private static final int RAFARCODE = 53;
 	private static final int TRAKARCODE = 52;
 	private static final int NUKTACODE = 51;
 
-	public MainLanguageExceptionHandler(Language lang){
+	public MainLanguageExceptionHandler(Language lang)
+	{
 		main = lang;
 		initializeKeyArray();
 	}
 
 	@SuppressLint("UseSparseArrays")
-	public HashMap<Integer, KeyAttr> handleException(int keyCode){
+	public HashMap<Integer, KeyAttr> handleException(int keyCode)
+	{
 		HashMap<Integer, KeyAttr> sKeys = new HashMap<Integer, KeyAttr>();
 		switch(keyCode){
 			case NUKTACODE:
@@ -48,7 +50,8 @@ public class MainLanguageExceptionHandler implements ExceptionHandler {
 		return sKeys;
 	}
 
-	private void initializeKeyArray(){
+	private void initializeKeyArray()
+	{
 		keyArray = new ArrayList<KeyAttr>();
 		int halantEnd = main.halantEnd;
 		for(int i = 0; i < halantEnd; i++){
@@ -78,7 +81,7 @@ public class MainLanguageExceptionHandler implements ExceptionHandler {
 	}
 
 	private void handleNukta(HashMap<Integer, KeyAttr> sKeys) {
-		int[] temp = {1,2,3,8,13,14,20,22,26,27,28};
+		int[] temp = {13,14};
 		//ArrayList<Integer> nuktaKeys = new ArrayList<Integer>();
 		SimpleArrayMap<Integer, String> nuktaKeyValues= new SimpleArrayMap<Integer, String>();
 
